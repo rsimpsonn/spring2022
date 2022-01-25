393c4ad4b397cdd51d4966ec25216c4ff9088df4
Riki Fameli <rikifameli@gmail.com>
2022-01-25 14:07:30 -0500

    commit before merging

--- assets/js/homepage_interactive.js
+++ assets/js/homepage_interactive.js
@@ -1,5 +1,5 @@
 /*
-This is a JS script to add interactive elements to the homepage of the website.
+This is a JS script to add interactive elements to the window containers on pages
 */
 
 function makeWindowsClosable() {
@@ -57,7 +57,6 @@ function deactivateDrag(allDraggableWindows) {
 function dragFunction(allDraggableWindows) {
     return (event) => {
         allDraggableWindows.forEach((aWindow) => {
-            // console.log(aWindow);
             if (aWindow.active) {
                 event.preventDefault();
                 if (event.type === "touchmove") {
@@ -74,24 +73,6 @@ function dragFunction(allDraggableWindows) {
         })
     }
 }
-// function dragFunction(itemToDrag) {
-//     return (event) => {
-//         console.log(itemToDrag.active)
-//         if (itemToDrag.active) {
-//             event.preventDefault();
-//             if (event.type === "touchmove") {
-//                 itemToDrag.currentX = event.touches[0].clientX - itemToDrag.initialX;
-//                 itemToDrag.currentY = event.touches[0].clientY - itemToDrag.initialY;
-//             } else {
-//                 itemToDrag.currentX = event.clientX - itemToDrag.initialX;
-//                 itemToDrag.currentY = event.clientY - itemToDrag.initialY;
-//             }
-//             itemToDrag.xOffset = itemToDrag.currentX;
-//             itemToDrag.yOffset = itemToDrag.currentY;
-//             setTranslate(itemToDrag.currentX, itemToDrag.currentY, itemToDrag.elt);
-//         }
-//     }
-// }
 
 function setTranslate(xPos, yPos, el) {
     console.log(el.style.transform)
@@ -113,66 +94,9 @@ function makeWindowsDraggable() {
     allDraggableWindows.forEach((draggableWindow) => {
         console.log(draggableWindow)
         draggableWindow.dragBar.addEventListener("touchstart", dragStartFunction(draggableWindow), false);
-        // draggableWindow.dragBar.addEventListener("touchend", deactivateDrag(allDraggableWindows), false);
-        // draggableWindow.dragBar.addEventListener("touchmove", dragFunction(draggableWindow), false);
-
         draggableWindow.dragBar.addEventListener("mousedown", dragStartFunction(draggableWindow), false);
-        // draggableWindow.dragBar.addEventListener("mouseup", deactivateDrag(allDraggableWindows), false);
-        // draggableWindow.dragBar.addEventListener("mousemove", dragFunction(draggableWindow), false);
-
     });
 }
 
-
 makeWindowsClosable()
 makeWindowsDraggable()
-
-
-// function dragStartFunction(draggableWindows) {
-//     return (event) => {
-//         draggableWindows.forEach((aWindow) => {
-//             if (aWindow.dragBar.contains(event.target)) {
-//                 if (event.type == "touchstart") {
-//                     aWindow.initialX = event.touches[0].clientX - aWindow.xOffset;
-//                     aWindow.initialY = event.touches[0].clientY - aWindow.yOffset;
-//                 } else {
-//                     aWindow.initialX = event.clientX - aWindow.xOffset;
-//                     aWindow.initialY = event.clientY - aWindow.yOffset;
-//                 }
-//                 if (aWindow.dragBar.contains(event.target)) {
-//                     aWindow.active = true;
-//                 }
-//             }
-//         })
-//     }
-// }
-
-// function dragEndFunction(draggableWindows) {
-//     return (event) => {
-//         draggableWindows.forEach((aWindow) => {
-//             if (aWindow.contains())
-//             aWindow.initialX = aWindow.currentX;
-//             aWindow.initialY = aWindow.currentY;
-//             aWindow.active = false;
-//         })
-//     }
-// }
-
-// function dragFunction(itemToDrag) {
-//     return (event) => {
-//         console.log(itemToDrag.active)
-//         if (itemToDrag.active) {
-//             event.preventDefault();
-//             if (event.type === "touchmove") {
-//                 itemToDrag.currentX = event.touches[0].clientX - itemToDrag.initialX;
-//                 itemToDrag.currentY = event.touches[0].clientY - itemToDrag.initialY;
-//             } else {
-//                 itemToDrag.currentX = event.clientX - itemToDrag.initialX;
-//                 itemToDrag.currentY = event.clientY - itemToDrag.initialY;
-//             }
-//             itemToDrag.xOffset = itemToDrag.currentX;
-//             itemToDrag.yOffset = itemToDrag.currentY;
-//             setTranslate(itemToDrag.currentX, itemToDrag.currentY, itemToDrag.elt);
-//         }
-//     }
-// }
